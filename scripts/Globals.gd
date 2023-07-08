3fbf71f0537e9d97f7362d458ee3ff455f18004a
Ivan Tsoninski <vankata@pop-os.localdomain>
2023-07-08 11:44:59 +0300

    added global script

---
+++ scripts/Globals.gd
@@ -0,0 +1,4 @@
+extends Node
+
+enum FOOD_PREFERENCE {POPSICLE, WAFFLE, CHOCOLATE}
+
---
+++ scripts/Kid.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+
+
+@export var food_preference = Globals.FOOD_PREFERENCE.CHOCOLATE
+
+func _physics_process(delta):
+	pass
