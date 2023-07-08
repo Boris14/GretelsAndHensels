7ef4ac0bc6e71f60c505b27eefd86b766cd5414c
Boris Dimitrov <boris1234dimitrov@gmail.com>
2023-07-08 12:28:09 +0300

    Pick up kid logic

--- scripts/BabaYaga.gd
+++ scripts/BabaYaga.gd
@@ -1,18 +1,48 @@
 extends CharacterBody2D
 
+signal ability_pressed(can_activate)
 
-const SPEED = 300.0
+const REACH_DIST = 8.0
+
+@export
+var normal_speed = 300.0
+@export
+var speed_carry_mult = 0.8
+@export
+var magic_regen = 2.0
+@export
+var strength = 5.0
+
+var _speed = normal_speed
+var _max_magic = 100.0
+var _curr_magic = 0.0
+var _is_carrying_kid = false
 
 func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	_speed = normal_speed * (speed_carry_mult if _is_carrying_kid else 1)
+	_curr_magic = clamp(_curr_magic + magic_regen, 0, _max_magic)
+	
+	# Movement
 	var direction = get_viewport().get_mouse_position() - position
-	if direction.length() > SPEED:
-		velocity = direction.normalized() * SPEED
+	if direction.length() > REACH_DIST:
+		velocity = direction.normalized() * _speed
 	else:
 		velocity = direction
 
-	move_and_slide()
-	
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		# Try to pick up a kid
+		for body in $PickUpArea.get_overlapping_bodies():
+			if body.is_in_group("kids"):
+				body.grab(self)
+				_is_carrying_kid = true
+				break
+		pass
+	elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		# Open ability UI
+		ability_pressed.emit(_curr_magic >= _max_magic)
+
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit() 
+
+	move_and_slide()
